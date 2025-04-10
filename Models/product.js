5291b213e8f45dffdb7e10514d11a1cc54acdf07
Madhu kasabe <kasabeviraj@gmail.com>
2025-04-10 16:12:36 +0530

    main

--- Models/product.js
+++ Models/product.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 const ProductSchema=mongoose.Schema({
 productName:{type:String, required:true, unique:true},
 productPrice:{type:Number, required:true},
-productUnit:{type:Number, required:true},
+productUnit:{type:String, required:true},
 productDescription:{type:String, required:true},
 })
 module.exports=mongoose.model('Product',ProductSchema)
